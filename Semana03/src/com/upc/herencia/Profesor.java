b5ddf0f862f12cc00a1f615301d3e3a59f4563f9
Carlos Alberto <pciscflo@upc.edu.pe>
2021-08-20 20:44:47 -0500

    20.08.03

---
+++ Semana03/src/com/upc/herencia/Profesor.java
@@ -0,0 +1,51 @@
+package com.upc.herencia;
+
+public class Profesor {
+    private String dni;
+    private String nombre;
+    private int edad;
+
+    public Profesor(String dni, String nombre, int edad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public double calcularSueldo(){ //método polimórfico
+        return 0;
+    }
+    public double calcularBono(){
+        double bono =0;
+        if(this.edad>=50){
+            bono = 100;
+        }
+        return bono;
+    }
+
+    public Profesor() {
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
