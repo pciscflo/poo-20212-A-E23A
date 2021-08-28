4a7d369a8d78c621db143d658e1eaf77a67a7ac2
Carlos Alberto <pciscflo@upc.edu.pe>
2021-08-28 08:09:34 -0500

    28.08.02

--- Semana04/src/com/upc/repetitivas/Ejercicios3.java
+++ Semana04/src/com/upc/repetitivas/Ejercicios3.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Ejercicios3 {
     public static void main(String[] args) {
         // suma = 1 + 4 + 7 +........n términos
-        int n, contadorTerminos=0, suma=0;
+        int n, contadorTerminos=1, suma=0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese n:");
-        n = sc.nextInt();
-        for(int i=1; contadorTerminos<n; i=i+3){//1,4,7
+        n = sc.nextInt();//2
+        for(int i=1; contadorTerminos<=n; i=i+3){//1,4   var = var + cte ---> contador
             //calcule la suma....
-            contadorTerminos = contadorTerminos + 1;//1,2
-            suma = suma + i;//1+4
+            contadorTerminos = contadorTerminos + 1;//2,3
+            suma = suma + i;//1+4  // var = var + variable  (acumulador)
         }
         System.out.println(suma);
 
